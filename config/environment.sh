3ccfabaeb3b496085f9a987268173f1128368e01
Nurmi Elisa A <elisanur@da5-cs-b221-22.pc.helsinki.fi>
2017-03-14 15:22:57 +0200

    eka testi

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
+USERNAME="elisanur"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER="postershop"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
